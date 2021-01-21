83a1fe8d0d8ce610eb3945156858a35dab9f139b
贾顺 <jiashun@outlook.com>
2021-01-21 11:37:42 +0800

    opt code

--- orm/orm.go
+++ orm/orm.go
@@ -3,22 +3,26 @@ package orm
 import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+	"log"
 )
+
 type User struct {
 	gorm.Model
 	Name string
-	Sex bool
-	Age int
+	Sex  bool
+	Age  int
 }
-func InitDatabase()  {
+
+func InitDatabase() {
 	db, err := gorm.Open("mysql", "root:mysqladmin@/test_jdbc?charset=utf8mb4&parseTime=True&loc=Local")
 	if err != nil {
-		panic(err)
+		log.Println(err)
 	}
 	db.AutoMigrate(&User{})
 	db.Where("name = ?", "jean").Find(&[]User{}).Updates(map[string]interface{}{
 		"Name": "rui",
-		"Sex": false,
+		"Sex":  false,
 	})
 	db.Where("name = ?", "shun").Find(&[]User{}).Unscoped().Delete(&[]User{})
+	defer db.Close()
 }
